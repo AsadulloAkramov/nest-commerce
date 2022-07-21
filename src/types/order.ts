a99ba7cb3e2819e6a392237557a08663042eeade
AsadulloAkramov <asadullo.2807@gmail.com>
2022-07-21 19:27:52 +0500

    some fix

--- src/types/order.ts
+++ src/types/order.ts
@@ -3,9 +3,13 @@ import { Document } from "mongoose";
 import { IProduct } from "./product";
 import { IUser } from "./user";
 
+interface IProductOrder {
+  product: IProduct,
+  quantity: number
+}
 export interface IOrder extends Document {
   owner: IUser,
   totalPrice: number,
-  products: IProduct,
+  products: IProductOrder[],
   created: Date
 }
